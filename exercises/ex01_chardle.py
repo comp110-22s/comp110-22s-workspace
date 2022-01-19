5f870090d0262ef74a729345b4181bdb2c007f95
Lixin Yang <lyang3@unc.edu>
2022-01-18 21:06:52 -0500

    Progress made in ex01 _2

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -5,23 +5,46 @@ word: str = input("Enter a 5-character word: ")
 character: str = input("Enter a single character: ")
 print("Searching for " + character + " in " + word)
 
+matching_characters: int = 0
 
 if character == word[0]:
     print(character + " found at index 0")
+    matching_characters = matching_characters + 1
 
 if character == word[1]:
     print(character + " found at index 1")
+    matching_characters = matching_characters + 1
+
+if character == word[2]:
+    print(character + " found at index 2")
+    matching_characters = matching_characters + 1
 
 if character == word[3]:
     print(character + " found at index 3")
+    matching_characters = matching_characters + 1
 
 if character == word[4]:
     print(character + " found at index 4")
+    matching_characters = matching_characters + 1
 
-if character == word[5]:
-    print(character + " found at index 5")
 
-if character == word[6]:
-    print(character + " found at index 6")
+if matching_characters == 1:
+    print(str(matching_characters) + " instance of " + character + " found in " + word)
+
+if matching_characters == 0:
+    print("No instances of " + character + " found in " + word)
+
+if matching_characters > 1:
+    print(str(matching_characters) + " instances of " + character + " found in " + word)
+    
+
+
+
+
+
+
+
+
+
 
 
