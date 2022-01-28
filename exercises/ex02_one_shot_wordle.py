982cb4debd122829ef9f486ad0b97c6eeea665e0
Naomi Tymann <ntymann@unc.edu>
2022-01-28 15:13:46 -0500

    Almost done with EX02 pt. 2!!

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -14,23 +14,19 @@ while len(user_guess) != len(secret_word):
 # of how long the secret word is, and it will get the user out of the error loop if they put in a guess the correct length, so
 # if they guess "python" at guess 3, they can get out of it and have their guiess evaluated by the yes/no responses below.
 
-if user_guess == secret_word:
-    print("Woo! You got it! ")
-
-if user_guess != secret_word:
-    print("Not quite. Play again soon! ")
-
 # start of pt. 2
 
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
+# How do I get the emoji string to concatenate all of the box emojis together after going through the while loop?
+
 guess_index: int = 0
 emoji: str = ""
 
 while guess_index < len(secret_word):
-    if user_guess[0] == secret_word[0]:
+    if user_guess[guess_index] == secret_word[guess_index]:
         print(GREEN_BOX)
     else:
         print(WHITE_BOX)
@@ -39,3 +35,9 @@ while guess_index < len(secret_word):
 print(emoji)
 
 # end of pt. 2
+
+if user_guess == secret_word:
+    print("Woo! You got it! ")
+
+if user_guess != secret_word:
+    print("Not quite. Play again soon! ")
