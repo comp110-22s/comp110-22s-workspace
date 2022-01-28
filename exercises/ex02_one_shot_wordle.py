e6790d80453098b699206dd2ce1d4cf82b7904c2
Naomi Tymann <ntymann@unc.edu>
2022-01-28 14:39:17 -0500

    Starting pt. 2

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -6,51 +6,36 @@ secret_word: str = "python"
 i: int = 0
 user_guess: str = input("What is your 6-letter guess? ")
 
-WHITE_BOX: str = "\U00002B1C"
-GREEN_BOX: str = "\U0001F7E9"
-YELLOW_BOX: str = "\U0001F7E8"
-
-# user_guess_index: int = (int(user_guess[0]))
-# emoji_box: str = ""
-
-# (start of pt. 2)
-
-# while user_guess_index < len(secret_word):
-#      if user_guess_index == secret_word[0]:
-#        print(GREEN_BOX)
-#    else:
-#        print(WHITE_BOX)
-#    user_guess_index = user_guess_index + 1
-#    print(emoji_box)
-
-# TEST THIS!!
-
-# (end of pt. 2)
-
-
-# (pt. 1 below)
-# python -m exercises.ex02_one_shot_wordle
-
-# During O.H.: 
-# How do I make it so that if the user guesses the secret word, then they get out of the loop before using 5 tries?
-# And how do I get the "that was not 6 letters" statement and the "try again" statement on the same line?
-
-retry: str = input("Try again: ")
-
-if len(user_guess) != len(secret_word):
-    print(f"That was not 6 letters! { retry }")
+while len(user_guess) != len(secret_word):
+    user_guess = input(f"That was not {len(secret_word)} letters! Try again: ")
     i = i + 1
-    while i < 4:
-        retry = input("Try again: ")
-        print(f"That was not 6 letters! { retry }")
-        i = i + 1
-    if i >= 4:
-        # and user_guess != secret_word:
-        print("Not quite. Play again soon! ")
-        quit()
+
+# This command will be able to do 2 things at once: it will check the length of the user guess compared to the secret word REGARDLESS
+# of how long the secret word is, and it will get the user out of the error loop if they put in a guess the correct length, so
+# if they guess "python" at guess 3, they can get out of it and have their guiess evaluated by the yes/no responses below.
 
 if user_guess == secret_word:
     print("Woo! You got it! ")
 
 if user_guess != secret_word:
     print("Not quite. Play again soon! ")
+
+# start of pt. 2
+
+WHITE_BOX: str = "\U00002B1C"
+GREEN_BOX: str = "\U0001F7E9"
+YELLOW_BOX: str = "\U0001F7E8"
+
+guess_index: int = 0
+emoji: str = ""
+
+while guess_index < len(secret_word):
+    if user_guess[0] == secret_word[0]:
+        print(GREEN_BOX)
+    else:
+        print(WHITE_BOX)
+    guess_index = guess_index + 1
+
+print(emoji)
+
+# end of pt. 2
