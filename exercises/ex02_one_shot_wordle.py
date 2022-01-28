92435f2a5a6258dbd07adf4eeff1ca0ec6de2578
Naomi Tymann <ntymann@unc.edu>
2022-01-28 12:27:47 -0500

    Almost done with pt. 1

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,24 +1,54 @@
-"""EX02 - One shot Wordle exercise"""
+"""EX02 - One shot Wordle exercise."""
 
 __author__ = "730466987"
 
 secret_word: str = "python"
-word_retry: int = 0
-
+i: int = 0
 user_guess: str = input("What is your 6-letter guess? ")
 
+WHITE_BOX: str = "\U00002B1C"
+GREEN_BOX: str = "\U0001F7E9"
+YELLOW_BOX: str = "\U0001F7E8"
+
+# user_guess_index: int = (int(user_guess[0]))
+# emoji_box: str = ""
+
+# (start of pt. 2)
+
+# while user_guess_index < len(secret_word):
+#      if user_guess_index == secret_word[0]:
+#        print(GREEN_BOX)
+#    else:
+#        print(WHITE_BOX)
+#    user_guess_index = user_guess_index + 1
+#    print(emoji_box)
+
+# TEST THIS!!
+
+# (end of pt. 2)
+
+
+# (pt. 1 below)
 # python -m exercises.ex02_one_shot_wordle
 
-if len(user_guess) != 6:
-    word_retry = word_retry + 1
-    while word_retry <= 4:
-        print("That was not 6 letters!")
-        input("Try again: ")
-        word_retry = word_retry + 1
+# During O.H.: 
+# How do I get the f-string format to work? And how do I get the "that was not 6 letters" statement and the "try again" statement 
+# on the same line?
+# Also, how do I make it so that if the user guesses the secret word, then they get out of the loop before using 5 tries?
 
-        if word_retry > 4:
-            print("Not quite. Play again soon! ")
-            quit()
+retry: str = input("Try again: ")
+
+if len(user_guess) != len(secret_word):
+    print("That was not 6 letters!")
+    i = i + 1
+    while i < 4:
+        retry = input("Try again: ")
+        print("That was not 6 letters!")
+        i = i + 1
+    if i >= 4:
+        # and user_guess != secret_word:
+        print("Not quite. Play again soon! ")
+        quit()
 
 if user_guess == secret_word:
     print("Woo! You got it! ")
