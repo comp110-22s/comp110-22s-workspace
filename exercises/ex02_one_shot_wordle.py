b1460f8160f1f8ab2ae8a032ef48fcd715fecf19
Hugh Toomey <htoomey@unc.edu>
2022-01-28 15:43:06 -0500

    fixed formatting

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -2,20 +2,20 @@
 
 __author__ = "730472535"
 
-secret_word = str("python")
+secret_word: str = "python"
 
-guess = input(str(f"What is your {len(secret_word)}-letter guess? "))
+guess: str = input(f"What is your {len(secret_word)}-letter guess? ")
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
-i = 0
-result = ""
-b = 0
+i: int = 0
+result: str = ""
+b: int = 0
 test: bool = False
 
 while len(guess) != len(secret_word):
-    guess = input(str(f"That in not {len(secret_word)} letters! Try again: "))
+    guess = input(f"That in not {len(secret_word)} letters! Try again: ")
     
 while i < len(secret_word):
     if guess[i] == secret_word[i]:
@@ -24,18 +24,18 @@ while i < len(secret_word):
         while not test and b < len(secret_word):  # b allowes the computer to test each letter of your guess against every letter in the secret word
             if guess[i] == secret_word[b]:
                 result = result + YELLOW_BOX
-                test: bool = True  # stops the while loop if letter exists in word
+                test = True  # stops the while loop if letter exists in word
                 b = 0  # resets the variable b in order to test the next letter correctly
             else:
                 if b == len(secret_word) - 1:  # program will only reach this step if the letter is not in the secret word
                     result = result + WHITE_BOX
                     b = 0
-                    test: bool = True  # exits the loop if the letter is not in the secret word
+                    test = True  # exits the loop if the letter is not in the secret word
                 else:
                     b += 1
                 
     i += 1  # ensures the loop is progressing forward
-    test: bool = False  # resets the bool value of test so loop can be run for the next letter
+    test = False  # resets the bool value of test so loop can be run for the next letter
 print(result)
 
 if guess == secret_word:
