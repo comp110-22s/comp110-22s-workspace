a603e1d1dd2a29c5bc9dee9ff90ada1028e78462
Maxwell Bauman <maxwjohn@email.unc.edu>
2022-02-21 16:16:10 -0500

    ex 5 work

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,4 +1,4 @@
-"""not sure yet what this will be."""
+"""Utility functions that will be tested on."""
 
 __author__ = "730405989"
 
@@ -9,7 +9,17 @@ def only_evens(a: list) -> list:
     number: int = a[i]
     for number in a:
         if number % 2 == 0:
-            empty_list.append(number)
-        i += 1
+            empty_list.append(int(number))
     return empty_list
 
+
+def sub(fullset: list, start: int, end: int) -> list:
+    i = 0
+    empty_list2: list = list()
+    nums: int = fullset[i]
+    if len(fullset) > 0:
+        for nums in fullset:
+            empty_list2.append(int(nums))
+        return empty_list2
+    else:
+        return empty_list2
