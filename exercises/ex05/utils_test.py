17ff2c9e0607a71a630e0adf409fd2725ae3b8cf
Maxwell Bauman <maxwjohn@email.unc.edu>
2022-02-22 21:41:57 -0500

    finish ex05

--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -2,22 +2,24 @@
 
 __author__ = "730405989"
 
-# from pyparsing import empty
 from exercises.ex05.utils import sub, concat, only_evens
 
 
 # First two tests making sure begin and end values working correctly
 
-def test_endcases() -> None:
+def test_boundcases() -> None:
+    """Test to check when start and end are less or greater than begin or start index."""
     assert sub([1, 2, 3], -5, 5) == [1, 2, 3]
 
 
 # Next test to address empty case
 def test_empty() -> None:
+    """Test when indices return zero values."""
     assert sub([1, 2, 3], -10, -1) == []
 
 
 def test_unit_case() -> None:
+    """Test for average case with varying numbers."""
     randlist: list = [1, 4, 10, 33, 2]
     assert sub(randlist, 2, 4) == [10, 33]
 
@@ -41,18 +43,21 @@ def test_regularex() -> None:
 
 
 def test_bothempty() -> None:
+    """Test when both lists are empty."""
     emptyconcat1: list = []
     emptyconcat2: list = []
     assert concat(emptyconcat1, emptyconcat2) == []
 
 
 def test_oneempty() -> None:
+    """Test when one is empty and the other is not."""
     empty1: list = []
     reg2: list = [1, 2, 5]
     assert concat(empty1, reg2) == [1, 2, 5]
 
 
 def test_concat() -> None:
+    """Test for example 2 lists."""
     a_list: list = [1, 4, -5, 7, 100]
     b_list: list = [3, 6]
     assert concat(a_list, b_list) == [1, 4, -5, 7, 100, 3, 6]
