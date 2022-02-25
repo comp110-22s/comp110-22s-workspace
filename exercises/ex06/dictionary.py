a674422dd38938a79cd5d819ef2814a1bed5c6ff
Naomi Tymann <ntymann@unc.edu>
2022-02-25 15:31:47 -0500

    EX06 pt. 0

---
+++ exercises/ex06/dictionary.py
@@ -0,0 +1,4 @@
+"""This is a practice project for working with dictionaries."""
+
+__author__: str = "730466987"
+
---
+++ exercises/ex06/dictionary_test.py
@@ -0,0 +1,4 @@
+"""These are test that evaluate dictionaries."""
+
+__author__: str = "730466987"
+
