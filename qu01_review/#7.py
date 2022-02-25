ab9f9d85d8ee2f450ca8cc82c43373d1ce3389fa
Naomi Tymann <ntymann@unc.edu>
2022-02-25 16:17:39 -0500

    Correcting #7

---
+++ qu01_review/#7.py
@@ -0,0 +1,16 @@
+"""This is a practice run-through of question 7 from QU01!"""
+
+
+def repeat(word: str, multip: int) -> str:
+    """This function repeats each letter in "word" "multip" times."""
+    i: int = 0
+    repeated: str = ""
+
+    while i < len(word):
+        si: int = 0
+
+        while si < multip:
+            repeated += word[i] 
+            si += 1
+        i += 1
+    return repeated
