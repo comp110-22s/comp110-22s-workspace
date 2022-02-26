e2289da0cece6b5366f8720e6513155552cde0e4
Naomi Tymann <ntymann@unc.edu>
2022-02-26 16:59:19 -0500

    Working on pts. 1 + 2

--- exercises/ex06/dictionary.py
+++ exercises/ex06/dictionary.py
@@ -6,18 +6,38 @@ __author__: str = "730466987"
 
 # Beginning of pt. 1:
 
-def invert(dict[str, str]) -> dict():
+# In O.H., how to you refer to the value part of the input? (See attempted usage in line ____.)
+
+def invert(input: dict[str, str]) -> dict{str, str}:
     """This function inverts the keys and values of the input list."""
-    
-    # Remember to add a KeyError message if the user tries to access a key that doesn't exist!
+    i: int = 0
+    switched: dict[str, str] = dict()
+
+    while i < len(input):
+        # switched = [value part of the input : key part of the input] <-- all at input[i]
+        i += 1
+    return switched
 
 # End of pt. 1
 
+
 # Beginning of pt. 2:
 
+# Also, how can I track which 
 
+def favorite_color(colors: dict[str, str]) -> str:
+    """This function returns the most common color value in its input list."""
+    i: int = 0
+    favorite: str = ""
+
+    while i < len(colors):
+        for color in colors:
+            # favorite += (the most common color within the length of the dictionary)
+    return favorite
+       
 
 # End of pt. 2
+
 
 # Beginning of pt. 3:
 
--- exercises/ex06/dictionary_test.py
+++ exercises/ex06/dictionary_test.py
@@ -6,45 +6,53 @@ __author__: str = "730466987"
 
 from exercises.ex06.dictionary import invert, favorite_color, count
 
-def_test_invert_____() -> None:
-    """This test _____________. (Edge case.)"""
-    assert invert(__?__) == ______
+
+def test_invert_dupl_value() -> None:
+    """This test prevents the invert function from returning duplicate values. (Use case.)"""
+    dupl_list: dict[str, str] = {'one': '1', 'two': '1'}
+    assert invert(dupl_list) == print("raise KeyError, please eliminate duplicate values.")
 
 
-def_test_invert_____() -> None:
-    """This test _____________. (Use case.)"""
-    assert invert(__?__) == ______
+def test_invert_matching_names() -> None:
+    """This test prevents invert from returning a set of key and value inputs with the same name. (Edge case.)"""
+    same_name_list: dict[str, str] = {"x": "x"}
+    assert invert(same_name_list) != same_name_list
 
 
-def_test_invert_____() -> None:
-    """This test _____________. (Use case.)"""
-    assert invert(__?__) == ______
+def test_invert_blank_name() -> None:
+    """This test prevents invert from returning a key or value input with a blank name. (Use case.)"""
+    blank_dict: dict[str, str] = {"z" : ""}
+    assert invert(blank_dict) != blank_dict
 
 
+# In O.H. (once all 3 functions are written out PLZ), see if you can fix this test so line 33 can do what the text says.
 
-def_test_favorite_color_____() -> None:
-    """This test _____________. (Edge case.)"""
-    assert favorite_color(__?__) == ______
-
-
-def_test_favorite_color_____() -> None:
-    """This test _____________. (Use case.)"""
-    assert favorite_color(__?__) == ______
+def test_favorite_color_tie() -> None:
+    """This test returns the earlier key (color) in a dictionary if 2 colors are equally common. (Use case.)"""
+    tie_dict: dict[str, str] = {"Joe": "pink", "Jane" : "green"}
+    assert favorite_color(tie_dict) == # (however you can indicate that pink came first in the list)
 
 
-def_test_favorite_color_____() -> None:
-    """This test _____________. (Use case.)"""
-    assert favorite_color(__?__) == ______
+def test_favorite_color_red_and_blue() -> None:
+    """This test guarantees that favorite_color will return red as the most common color in the dictionary. (Use case.)"""
+    red_wins: dict[str, str] = {"Joe": "red", "Jane": "red", "Julia": "blue"}
+    assert favorite_color(red_wins) == print("red")
 
 
-def_test_count_____() -> None:
+def test_favorite_color_blank() -> None:
+    """This test prevents favorite_color from processing a list where one of the keys has a blank value. (Edge case.)"""
+    no_color: dict[str, str] = {"1": ""}
+    assert favorite_color(no_color) != {"1", ""}
+
+
+def test_count_____() -> None:
     """This test _____________. (Edge case.)"""
     assert count(__?__) == ______
 
-def_test_count_____() -> None:
+def test_count_____() -> None:
     """This test _____________. (Edge case.)"""
     assert count(__?__) == ______
 
-def_test_count_____() -> None:
+def test_count_____() -> None:
     """This test _____________. (Edge case.)"""
     assert count(__?__) == ______
