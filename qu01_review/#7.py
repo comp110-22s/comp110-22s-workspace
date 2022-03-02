f001ec9932b9eec594771a947aa8fd0c10808b7a
Naomi Tymann <ntymann@unc.edu>
2022-03-02 17:12:40 -0500

    Done reviewing QU01!

--- qu01_review/#7.py
+++ qu01_review/#7.py
@@ -1,16 +1,16 @@
 """This is a practice run-through of question 7 from QU01!"""
 
 
-def repeat(word: str, multip: int) -> str:
-    """This function repeats each letter in "word" "multip" times."""
+def indices_of(word: str, char: str) -> str:
+    """This function lists the indices in the input word where the input character appears."""
     i: int = 0
-    repeated: str = ""
+    all_indices: str = ""
 
-    while i < len(word):
-        si: int = 0
-
-        while si < multip:
-            repeated += word[i] 
-            si += 1
-        i += 1
-    return repeated
+    while i <= len(word):
+        if char == word[i]:
+            all_indices += char
+            i += 1
+        else:
+            all_indices = ""
+            i += 1
+    return all_indices
