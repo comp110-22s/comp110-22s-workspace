01c36d893cf525ba930da167bdf66735f988bdaf
Naomi Tymann <ntymann@unc.edu>
2022-03-09 01:36:26 -0500

    done

--- lessons/data_utils.py
+++ lessons/data_utils.py
@@ -32,6 +32,7 @@ def read_csv_rows(filename: str) -> list[dict[str, str]]:
 def column_values(table: list[dict[str, str]], column: str) -> list[str]:
     """Produce a list[str] of all vlaues in a single column."""
     result: list[str] = []
+    
     for row in table:
         item: str = row[column]
         result.append(item)
