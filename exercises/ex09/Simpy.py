e303d2b0ac0c600b759c14f16dc0836aaf60d273
Naomi <ntymann@unc.edu>
2022-04-02 01:16:18 -0400

    Started EX09

--- exercises/ex09/Simpy.py
+++ exercises/ex09/Simpy.py
@@ -4,10 +4,37 @@ from __future__ import annotations
 
 from typing import Union
 
-__author__ = "YOUR PID HERE"
+__author__ = "730466987"
 
 
 class Simpy:
     values: list[float]
 
-    # TODO: Your constructor and methods will go here.
+    def __str__()
+
+    def __init__(self, values: list[float]):
+        """Declares a new Simpy object."""
+        self.values = values
+
+    def fill(self, filled: float, fill_number: int) -> None:
+
+
+    def arange(self, start: float, stop: float, step: float) -> ___(?):
+
+    
+    def sum() -> float:
+
+
+    def __add__(lhs: Union[Simpy, float], rhs: Union[Simpy, float]) -> Simpy:
+
+
+    def __pow__(lhs: Union[Simpy, float], rhs: Union[Simpy, float]) -> Simpy:
+
+
+    def __eq__ (mask: list[bool], object_one: Union[Simpy, float]) -> list[bool]:
+
+    
+    def __gt__()
+
+
+    def __getitem__()
