6029cd0b776ace5b46a3e91ae6a8ad4dc014916e
Naomi <ntymann@unc.edu>
2022-04-03 18:15:38 -0400

    Worked on arange and sum

--- exercises/ex09/Simpy.py
+++ exercises/ex09/Simpy.py
@@ -21,17 +21,40 @@ class Simpy:
     def fill(self, filling: float, fill_number: int) -> None:
         """Fills a Simpy object's list with a set number of repetitions of a set value."""
         i: float = 0.0
-        self.values = []
+        self.values: list = []
 
         while i < fill_number:
             self.values.append(filling)
             i += 1.0
 
-    def arange(self, start: float, stop: float, step: float = 1.0):
-        """Returns the values within a set input."""
-        
+# In O.H.: how do you get "step" to be the equal amount of distance between each item in the input list, even if it increases by 0.25? (like it does with "fractional")
 
-    # def sum() -> float:
+    # def arange(self, start: float, stop: float, step: float = 1.0):
+    #     """Returns the values within a set input, increasing by "step" value each time."""
+    #     assert step != 0.0
+
+    #     self.values: list = [start]
+    #     i: float = 1.0
+
+    #     step = (stop/start)
+
+    #     while i < stop:
+    #         self.values. append(start + step)
+    #         step += 1.0
+    #         i += 1
+
+# NOTE: once you fix "arange," this function should return 45 instead of 55!
+
+    def sum(self) -> float:
+        """Sums all of the items in each index of the input Simpy."""
+        summed: float = 0.0
+        i: int = 0
+
+        while i < len(self.values):
+            summed += self.values[i]
+            i += 1
+
+        return summed
 
     # def __add__(lhs: Union[Simpy, float], rhs: Union[Simpy, float]) -> Simpy:
 
