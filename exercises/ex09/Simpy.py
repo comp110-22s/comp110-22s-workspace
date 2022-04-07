f8e9c212127d810f68792785382bd1f7d9b540e6
Naomi <ntymann@unc.edu>
2022-04-06 22:39:06 -0400

    Finished getitem pt. 1!

--- exercises/ex09/Simpy.py
+++ exercises/ex09/Simpy.py
@@ -18,30 +18,28 @@ class Simpy:
         """Converts a Simpy object to a str object."""
         return f"Simpy: ({self.values})"
 
+# In O.H.: figure out how to properly mutate "filling" so that it repeats "fill_number" times!
+
     def fill(self, filling: float, fill_number: int) -> None:
         """Fills a Simpy object's list with a set number of repetitions of a set value."""
-        i: float = 0.0
-        final_list: list = []
+        return filling ** fill_number
 
-        while i < fill_number:
-            final_list.append(filling)
-            i += 1.0
 
-# In O.H.: how do you get "step" to be the equal amount of distance between each item in the input list, even if it increases by 0.25? (like it does with "fractional")
+# Also inn O.H.: how do you get "step" to be the equal amount of distance between each item in the input list, even if it increases by a fraction?
 
-    # def arange(self, start: float, stop: float, step: float = 1.0):
-    #     """Returns the values within a set input, increasing by "step" value each time."""
-    #     assert step != 0.0
+    def arange(self, start: float, stop: float, step: Union[None, float]????????):
+        """Returns the values within a set input, increasing by "step" value each time."""
+        step = (stop / start)
+        assert step != 0.0
+        
+        result: list = [start]
+        i: float = 1.0
 
-    #     self.values: list = [start]
-    #     i: float = 1.0
-
-    #     step = (stop/start)
-
-    #     while i < stop:
-    #         self.values. append(start + step)
-    #         step += 1.0
-    #         i += 1
+        while i < stop:
+            self.values. append(start + step)
+            step += 1.0
+            i += 1
+        return result
 
 # NOTE: once you fix "arange," this function should return 45 instead of 55!
 
@@ -55,8 +53,6 @@ class Simpy:
             i += 1
 
         return summed
-
-# In O.H., figure out why the Jupyter notebook says that Simpy objects are not defined for this method!
 
     def __add__(self, rhs: Union[float, Simpy]) -> Simpy:
         """Adds Simpy and float objects together."""
@@ -110,7 +106,7 @@ class Simpy:
             return matching
 
     def __gt__(self, rhs: Union[float, Simpy]) -> list[bool]:
-        """"Overload that returns the values in an input value that are greater than the items at the same indices in rhs."""
+        """Overload that returns the values in an input value that are greater than the items at the same indices in rhs."""
         greater_than: list[bool] = []
 
         if isinstance(rhs, Simpy):
